504d9077710660197d8f0a9d6709401d24eb0b74
EddWills95 <edd.williams@me.com>
2021-04-07 20:22:10 +0100

    update sorted to use ratio

--- client/src/utils/sortChannels.js
+++ client/src/utils/sortChannels.js
@@ -1,19 +1,15 @@
-const sorters = ['local_balance', 'remote_balance'];
-
-const sortChannels = (channels, key = "local_balance") => {
-    if (!sorters.includes(key)) {
-        return channels;
-    }
-
-    const calculatePercentage = (channel) => {
-        return (channel[key] / channel.capacity) * 100;
-    }
-
-    return channels.sort((a, b) => {
-        if (calculatePercentage(a) < calculatePercentage(b)) return -1;
-        if (calculatePercentage(a) > calculatePercentage(b)) return 1;
+const sortChannels = (channels, reverse = false) => {
+    const sorted = channels.sort((a, b) => {
+        if (a.localRatio < b.localRatio) return -1;
+        if (a.localRatio > b.localRatio) return 1;
         return 0;
-    })
+    });
+
+    if (reverse) {
+        return sorted.reverse();
+    }
+
+    return sorted;
 }
 
 export default sortChannels;
