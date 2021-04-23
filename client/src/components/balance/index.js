8b6f067c1173ce4a298d74fc049b9bcb0c0fd6ec
EddWills95 <edd.williams@me.com>
2021-04-23 22:08:53 +0100

    add animating bar

--- client/src/components/balance/index.js
+++ client/src/components/balance/index.js
@@ -1,23 +1,22 @@
-import { memo } from "react";
+import { memo, useEffect, useState } from "react";
 import "./style.scss";
 
 const Balance = ({ remote_balance, local_balance }) => {
-    // calculate % widths for each side
+    const [outboundWidth, setOutboundWidth] = useState('0%');
+    const [inboundWidth, setInboundWidth] = useState('0%');
 
+    // calculate % widths for each side
     const capacity = remote_balance + local_balance;
 
-    const outboundStyle = {
-        width: `${(local_balance / capacity) * 100}%`
-    }
-
-    const inboundStyle = {
-        width: `${(remote_balance / capacity) * 100}%`
-    }
+    useEffect(() => {
+        setOutboundWidth(`${(local_balance / capacity) * 100}%`);
+        setInboundWidth(`${(remote_balance / capacity) * 100}%`)
+    }, []);
 
     return (
         <div className="balance">
-            <div className="outbound" style={{ ...outboundStyle }} />
-            <div className="inbound" style={{ ...inboundStyle }} />
+            <div className="outbound" style={{ width: outboundWidth }} />
+            <div className="inbound" />
         </div>
     );
 }
