ada090925063711d6438051443db783a1221d6e0
EddWills95 <edd.williams@me.com>
2021-10-16 21:53:19 +0100

    add some nice styling when rebalancing

---
+++ client/src/components/message/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Message = ({ message, ...props }) => {
+    const messageClasses = (message = '') => {
+        console.log(message);
+        let classes = ['message'];
+        if (message.includes('Trying route')) {
+            classes.push('trying-route');
+        }
+
+        if (message.includes('Temporary channel failure')) {
+            classes.push('channel-failure');
+        }
+
+        if (message.match(/([0-9]+ to (.)*)/) && !message.includes('Skipping')) {
+            classes.push('channel-path');
+        }
+
+        if (message.includes('Ignoring')) {
+            classes.push('ignoring-path')
+        }
+
+        if (message.includes('Could not find any suitable route')) {
+            classes.push('no-route');
+        }
+        return classes.join(' ');
+    }
+
+    return <p {...props} className={messageClasses(message)}>{message}</p>
+}
+
+export default Message;
